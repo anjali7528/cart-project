2db3a39407322623c88a9760865f9e9f1845fccf
anjali7528 <anjaligandhi248@gmail.com>
2022-02-22 23:49:19 +0530

    new commit

--- src/Cart.js
+++ src/Cart.js
@@ -2,14 +2,43 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            product: [
+         {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: '',
+            id:2 
+        },
+        {
+            price: 99,
+            title: 'Watch',
+            qty: 10,
+            img: '',
+            id:3
+        }
+    ]
+        //this.increaseQuantity = this.increaseQuantity.bind(this);
+        //this.testing();
+}
+    }
    
     render() {
+        const {products} =this.state;
         return(
            <div className="cart" >
-           <CartItem />
-           <CartItem />
-           <CartItem />
-           <CartItem />
+          
+          {products.map((product) =>{
+          return <CartItem product={product} key={product.id}/>
+          })}
+           {/* {
+               arr.map((item) => {
+                   return item+5
+               })
+           } */}
            </div>
         );
     }
